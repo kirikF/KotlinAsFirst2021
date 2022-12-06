f8e8199600b5bfa6ff45b69ef68315b7a671cf0f
kirikF <112721634+kirikF@users.noreply.github.com>
2022-12-07 00:50:14 +0300

    Update Logical.kt

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -72,7 +72,7 @@ fun daysInMonth(month: Int, year: Int): Int {
 fun circleInside(
     x1: Double, y1: Double, r1: Double,
     x2: Double, y2: Double, r2: Double
-): Boolean = TODO()
+): Boolean = sqrt(sqr(y1 - y2) + sqr(x1 - x2)) + r1 <= r2
 
 /**
  * Средняя (3 балла)
