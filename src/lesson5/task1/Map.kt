e079238ed89e611da2032168a2ce61eaa3bbbc4b
kirikF <112721634+kirikF@users.noreply.github.com>
2022-12-13 23:03:29 +0300

    Update Map.kt

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -299,7 +299,18 @@ fun propagateHandshakes(friends: Map<String, Set<String>>): Map<String, Set<Stri
  *   findSumOfTwo(listOf(1, 2, 3), 4) -> Pair(0, 2)
  *   findSumOfTwo(listOf(1, 2, 3), 6) -> Pair(-1, -1)
  */
-fun findSumOfTwo(list: List<Int>, number: Int): Pair<Int, Int> = TODO()
+fun findSumOfTwo(list: List<Int>, number: Int): Pair<Int, Int> {
+    val result = mutableMapOf<Int, Int>()
+    if (list.isNotEmpty()) {
+        for (i in list.indices) {
+            if (result.containsKey(number - list[i])) {
+                return Pair(result[number - list[i]]!!, i)
+            }
+            result[list[i]] = i
+        }
+    }
+    return Pair(-1, -1)
+}
 
 /**
  * Очень сложная (8 баллов)
