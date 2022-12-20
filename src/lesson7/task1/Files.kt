158e5be99f117d98510834ad33f953fd01f135c3
kirikF <112721634+kirikF@users.noreply.github.com>
2022-12-21 00:18:19 +0300

    Update Files.kt

--- src/lesson7/task1/Files.kt
+++ src/lesson7/task1/Files.kt
@@ -117,8 +117,8 @@ fun sibilants(inputName: String, outputName: String) {
  */
 fun centerFile(inputName: String, outputName: String) {
     val lines = File(inputName).readLines().map { it.trim() }
-    val maxLength = lines.maxOf { it.length }
     if (lines.isNotEmpty()) {
+        val maxLength = lines.maxOf { it.length }
         File(outputName).bufferedWriter().use {
             for (line in lines) {
                 val spaces = (maxLength - line.length) / 2
